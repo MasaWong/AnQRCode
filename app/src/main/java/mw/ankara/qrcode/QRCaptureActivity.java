1237ced9cccb2dfd7ad3c801564ec54820ac9275
masawong <masawong@163.com>
2015-10-03 22:43:09 +0800

    qr code

---
+++ app/src/main/java/mw/ankara/qrcode/QRCaptureActivity.java
@@ -0,0 +1,202 @@
+package mw.ankara.qrcode;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import mw.ankara.qrcode.camera.CameraManager;
+import mw.ankara.qrcode.decoding.CaptureActivityHandler;
+import mw.ankara.qrcode.decoding.InactivityTimer;
+import mw.ankara.qrcode.view.QRCaptureView;
+
+public class QRCaptureActivity extends AppCompatActivity implements Callback {
+
+    private static final float BEEP_VOLUME = 0.10f;
+
+    private QRCaptureView mQRCaptureView;
+
+    private CaptureActivityHandler mCaptureActivityHandler;
+    private Vector<BarcodeFormat> mBarcodeFormats;
+    private InactivityTimer mInactivityTimer;
+    private MediaPlayer mMediaPlayer;
+
+    private String mCharacterset;
+
+    private boolean mHasSurface;
+    private boolean mPlayBeep;
+    private boolean mVibrate;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_capture);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.qr_capture_tb_toolbar);
+        setSupportActionBar(toolbar);
+
+        CameraManager.init(getApplication());
+        mQRCaptureView = (QRCaptureView) findViewById(R.id.qr_capture_qcv_finder);
+        mInactivityTimer = new InactivityTimer(this);
+
+        mHasSurface = false;
+
+        mVibrate = true;
+        AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+        mPlayBeep = audioService.getRingerMode() == AudioManager.RINGER_MODE_NORMAL;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.qr_capture_sv_preview);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (mHasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+        }
+        mBarcodeFormats = null;
+        mCharacterset = null;
+
+        initBeepSound();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCaptureActivityHandler != null) {
+            mCaptureActivityHandler.quitSynchronously();
+            mCaptureActivityHandler = null;
+        }
+        CameraManager.get().closeDriver();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mInactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        try {
+            CameraManager.get().openDriver(surfaceHolder);
+        } catch (IOException | RuntimeException ioe) {
+            return;
+        }
+
+        if (mCaptureActivityHandler == null) {
+            mCaptureActivityHandler = new CaptureActivityHandler(this, mBarcodeFormats, mCharacterset);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!mHasSurface) {
+            mHasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHasSurface = false;
+
+    }
+
+    public QRCaptureView getQRCaptureView() {
+        return mQRCaptureView;
+    }
+
+    public Handler getCaptureActivityHandler() {
+        return mCaptureActivityHandler;
+    }
+
+    public void drawViewfinder() {
+        mQRCaptureView.drawViewfinder();
+    }
+
+    public void handleDecode(final Result obj, Bitmap barcode) {
+        mInactivityTimer.onActivity();
+        playBeepSoundAndVibrate();
+
+        try {
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            Uri uri = Uri.parse(obj.getText());
+            intent.setData(uri);
+            startActivity(intent);
+            finish();
+        } catch (ActivityNotFoundException ignored) {
+        }
+    }
+
+    private void initBeepSound() {
+        if (mPlayBeep && mMediaPlayer == null) {
+            // The volume on STREAM_SYSTEM is not adjustable, and users found it
+            // too loud,
+            // so we now play on the music stream.
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setOnCompletionListener(beepListener);
+
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.beep);
+            try {
+                mMediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+                file.close();
+                mMediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
+                mMediaPlayer.prepare();
+            } catch (IOException e) {
+                mMediaPlayer = null;
+            }
+        }
+    }
+
+    private static final long VIBRATE_DURATION = 200L;
+
+    private void playBeepSoundAndVibrate() {
+        if (mPlayBeep && mMediaPlayer != null) {
+            mMediaPlayer.start();
+        }
+        if (mVibrate) {
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            vibrator.vibrate(VIBRATE_DURATION);
+        }
+    }
+
+    /**
+     * When the beep has finished playing, rewind to queue up another one.
+     */
+    private final OnCompletionListener beepListener = new OnCompletionListener() {
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
+}
---
+++ app/src/main/java/mw/ankara/qrcode/view/QRCreatorView.java
@@ -0,0 +1,93 @@
+package mw.ankara.qrcode.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.HashMap;
+
+/**
+ * 生成二维码的ImageView
+ *
+ * @author masawong
+ * @since 10/3/15
+ */
+public class QRCreatorView extends ImageView {
+
+    public QRCreatorView(Context context) {
+        super(context);
+    }
+
+    public QRCreatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public QRCreatorView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public QRCreatorView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void showQrCode(String message, OnQrCodeCreatedListener listener) {
+        // 字符不能为空
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+
+        try {
+            HashMap<EncodeHintType, String> hints = new HashMap<>(1);
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+
+            //图像数据转换，使用了矩阵转换
+            final int width = getMeasuredWidth();
+            final int height = getMeasuredHeight();
+            BitMatrix rawBitMatrix = new QRCodeWriter().encode(
+                message, BarcodeFormat.QR_CODE, width, height, hints);
+
+            //下面这里按照二维码的算法，逐个生成二维码的图片，
+            int[] pixels = new int[width * height];
+            //两个for循环是图片横列扫描的结果
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    if (rawBitMatrix.get(x, y)) {
+                        pixels[y * width + x] = 0xff000000;
+                    } else {
+                        pixels[y * width + x] = 0xffffffff;
+                    }
+                }
+            }
+
+            //生成二维码图片的格式，使用ARGB_8888
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+            //显示到一个ImageView上面
+            setImageBitmap(bitmap);
+
+            if (listener != null) {
+                listener.onSuccess(bitmap);
+            }
+        } catch (Exception e) {
+            if (listener != null) {
+                listener.onFailed(e);
+            }
+        }
+    }
+
+    public interface OnQrCodeCreatedListener {
+        void onSuccess(Bitmap bitmap);
+        void onFailed(Exception e);
+    }
+}
